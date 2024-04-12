96084c6f468e3b0b86092e1c537a483a355d38a5
Benhin <benhinmwendwa7@gmail.com>
2024-04-12 16:27:34 +0300

    form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form = document.querySelector("create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+  buildToDoList(e.target.text.value);
+  form.reset()
+  })
 });
+function buildToDoList(todo){
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", Delete)
+  btn.textContent = "Delete";
+  p.textContent = `${todo}`
+  p.appendChild(btn);
+  document.querySelector("#list").appendChild(btn);
+
+}
+function Delete(e) {
+  e.target.parentNode.remove();}
